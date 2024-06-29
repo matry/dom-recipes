b1997f6374f6d21cdcf8af3efc3cfcb50d228c62
Daniel Vaughn <danielbvaughn@gmail.com>
2024-06-28 21:33:47 -0400

    adds todo recipe

---
+++ recipes/todo/todo.jsx
@@ -0,0 +1,93 @@
+import { write, append, setAttributes, empty } from '@matry/dom';
+
+const model = {
+  todos: [],
+}
+
+write('app', (
+  <div id="todo-container">
+    <h2>Your ToDos</h2>
+    <form
+      onsubmit={onFormSubmit}
+    >
+      <input
+        type="text"
+        id="todo-input"
+        name="todo-input"
+        value=""
+        placeholder="What do you want to achieve today?"
+        style="width: 250px;"
+      />
+      <button type="submit">Add</button>
+    </form>
+
+    <ul id="todo-list">
+      {model.todos.map((todo) => {
+        return (
+          <li id={todo.id}>{todo.description}</li>
+        )
+      })}
+    </ul>
+    <button
+      id="clear"
+      onclick={onClearClick}
+    >
+      clear all
+    </button>
+  </div>
+));
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  const formData = new FormData(this);
+  const value = formData.get('todo-input');
+
+  if (value !== null) {
+    const newToDo = {
+      id: `todo-n-${model.todos.length + 1}`,
+      description: value,
+      done: false,
+    };
+
+    model.todos.push(newToDo);
+
+    didAddToDo(newToDo);
+  }
+}
+
+function onClearClick() {
+  model.todos = [];
+
+  didClearToDos();
+}
+
+function didAddToDo(todo) {
+  append(
+    <ul id="todo-list">
+      <li
+        class="todo-item"
+        onclick={() => {
+          setAttributes(
+            <div
+              id={todo.id}
+              class="done"
+            />
+          )
+        }}
+      >
+        <div id={todo.id} />
+        <span>{todo.description}</span>
+      </li>
+    </ul>
+  )
+  setAttributes(
+    <input id="todo-input" value="" />
+  )
+}
+
+function didClearToDos() {
+  empty(
+    <ul id="todo-list" />
+  )
+}
